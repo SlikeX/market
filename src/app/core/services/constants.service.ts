9cb8ed338e1b8de0f2d75866e4eb33c584fb586c
Vyacheslav_Maksyutov <vyacheslav_maksyutov@epam.com>
2023-01-18 20:04:22 +0500

    feat: add constants service

---
+++ src/app/core/services/constants.service.ts
@@ -0,0 +1,9 @@
+import {InjectionToken} from "@angular/core";
+import {ConstantsModel} from "../models/constants.model";
+
+export const constants = {
+  App: 'TaskManager',
+  Ver: '1.0',
+  API_URL: 'http://taskmanager.epam.com',
+}
+export const CONSTANTS = new InjectionToken<ConstantsModel>('info')
